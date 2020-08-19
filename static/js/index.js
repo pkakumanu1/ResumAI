69c0038e07a2300e85b6f3d09e4d36d4fddb0ea1
pkakumanu1 <61473104+pkakumanu1@users.noreply.github.com>
2020-08-19 14:04:57 -0400

    Updates

--- static/js/index.js
+++ static/js/index.js
@@ -72,11 +72,11 @@ function fileInputControlChangeEventHandler(event) {
 function analyseCV(data) {
   alert("I am inside analyseCV");
   $.support.cors = true;
-  $.mobile.allowCrossDomainPages = true;
-  $.mobile.phonegapNavigationEnabled = true;
+  // $.mobile.allowCrossDomainPages = true;
+  // $.mobile.phonegapNavigationEnabled = true;
 
-  $.mobile.linkBindingEnabled = true;
-  $.mobile.ajaxEnabled = true;
+  // $.mobile.linkBindingEnabled = true;
+  // $.mobile.ajaxEnabled = true;
   // var settings = {
   //   "url": "https://gateway.watsonplatform.net/natural-language-understanding/api/v1/analyze?version=2019-07-12",
   //   "method": "POST",
@@ -131,10 +131,11 @@ function analyseCV(data) {
           var col = row.append('td').text(record['confidence'])
   
         }
-  }).fail(function(xhr, status, error){
-    alert("You got error", xhr, status, error)
-    console.log(xhr,status,error);
-  });
+  })
+  // .fail(function(xhr, status, error){
+  //   alert("You got error", xhr, status, error)
+  //   console.log(xhr,status,error);
+  // });
   sortTable(); 
   });
 
